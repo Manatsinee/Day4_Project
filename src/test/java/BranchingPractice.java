f7db7013fe5dc98d2e5195e3d63f4cc45e5944d3
Manatsinee <littlebring@gmail.com>
2021-05-25 19:50:54 -0400

    initial commit

---
+++ src/test/java/BranchingPractice.java
@@ -0,0 +1,11 @@
+public class BranchingPractice {
+    public static void main(String[] args) {
+        //created new project
+        // enabled version control
+        // added .gitIgnore with .idea, target folder and.iml file
+        // made initial commit by keeping track of all file
+        // shared project on github
+
+
+    }
+}
